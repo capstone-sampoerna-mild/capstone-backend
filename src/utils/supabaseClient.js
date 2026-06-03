a6095457013cb9a3eef2e3f54cfd288f8850f6f4
arbasyaa <arbasyaa@gmail.com>
2026-06-04 03:56:28 +0700

    update-ws

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,12 +1,17 @@
 import { createClient } from '@supabase/supabase-js';
 import { config } from '../config/environment.js';
+import ws from 'ws';
 
 if (!config.supabase.url || !config.supabase.serviceRoleKey) {
   throw new Error('Supabase configuration is missing');
 }
 
+
 export const supabase = createClient(config.supabase.url, config.supabase.serviceRoleKey, {
   auth: {
     persistSession: false,
+  },
+  realtime: {
+    transport: ws,
   },
 });
